270c7b009e9bbfa7c6b342cf5928244739f4e3e0
Rexbanana1 <b30781@student.ghchs.com>
2018-01-11 14:41:47 -0800

    Started Lift -Brandon

--- src/Lift.cpp
+++ src/Lift.cpp
@@ -5,6 +5,48 @@
  *      Author: Admin
  */
 
+#ifdef LIFT_CODE
+
+#include "WPILib.h"
+#include "Lift.h"
+
+
+Lift::Lift()
+{
+	motorOne = new CANTalon(1);
+	motorTwo = new CANTalon(1);
+	motorThree = new CANTalon(1);
+	motorFour = new CANTalon(1);
+	motorFive = new CANTalon(1);
+	motorSix = new CANTalon(1);
+	motorSeven = new CANTalon(1);
+	motorEight = new CANTalon(1);
+}
+
+Lift::~Lift()
+{
+	delete motorOne;
+	delete motorTwo;
+	delete motorThree;
+	delete motorFour;
+	delete motorFive;
+	delete motorSix;
+	delete motorSeven;
+	delete motorEight;
+
+	motorOne = nullptr;
+	motorTwo = nullptr;
+	motorThree = nullptr;
+	motorFour = nullptr;
+	motorFive = nullptr;
+	motorSix = nullptr;
+	motorSeven = nullptr;
+	motorEight = nullptr;
+
+
+}
+
+
 
 
 
--- src/Lift.h
+++ src/Lift.h
@@ -4,12 +4,26 @@
  *  Created on: Jan 6, 2018
  *      Author: Admin
  */
+#include "CANTalon.h"
 
 #ifndef SRC_LIFT_H_
 #define SRC_LIFT_H_
 
 
+using namespace std;
 
+class Lift
+{
+public:
+		Lift();
+		~Lift();
+
+private:
+		CANTalon* motorOne;
+
+
+
+};
 
 
 #endif /* SRC_LIFT_H_ */
